1584898332e11dd1a00c294e700a8859cfa782a5
oalonsobu <oalonsobu@uoc.edu>
2019-05-05 12:14:15 +0200

    Basic parallax effect, need to ajust the layer to the bottom of the screen

---
+++ Shiane/Assets/Scripts/GameControllers/ParallaxController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxController : MonoBehaviour
+{
+    
+    private List<SpriteRenderer> parallaxLayers;
+    [SerializeField] GameObject backLayer;
+    [SerializeField] GameObject frontLayer;
+    Vector3 mainCameraLastPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCameraLastPosition = Camera.main.transform.position;
+        InitLayer(backLayer);
+        InitLayer(frontLayer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLayer(backLayer, 1f);
+        UpdateLayer(frontLayer, 2f);
+        
+        mainCameraLastPosition = Camera.main.transform.position;
+    }
+
+    bool IsInLeftSideScreen(float pos, float size)
+    {
+        //Get the most left and right point in the screen
+        float screenRatio = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float minX = Camera.main.transform.position.x - screenRatio;
+        return minX > pos + size;
+    }
+    
+    bool IsInRightSideScreen(float pos, float size)
+    {
+        //Get the most left and right point in the screen
+        float screenRatio = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float maxX = Camera.main.transform.position.x + screenRatio;
+        return maxX < pos - size;
+    }
+
+    void InitLayer(GameObject layer)
+    {
+        int counter = 0;
+        foreach (Transform child in layer.transform)
+        {
+            float extents = child.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float currentPos = 2 * extents * counter;
+            child.position = new Vector3(currentPos, layer.transform.position.y, layer.transform.position.z);
+            counter++;
+        }
+    }
+
+    void UpdateLayer(GameObject layer, float speed)
+    {
+        float cameraDistanceTravelled = mainCameraLastPosition.x - Camera.main.transform.position.x;
+        foreach (Transform child in layer.transform)
+        {
+            float extents = child.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float currentPos = child.position.x;
+            currentPos += Time.deltaTime * speed * cameraDistanceTravelled;
+            if (IsInLeftSideScreen(currentPos, extents) && cameraDistanceTravelled < 0)
+            {
+                currentPos += 4 * extents;
+            } else if (IsInRightSideScreen(currentPos, extents) && cameraDistanceTravelled > 0)
+            {
+                currentPos -= 4 * extents;
+            }
+            child.position = new Vector3(currentPos, child.position.y, child.position.z);
+        }
+    }
+}
