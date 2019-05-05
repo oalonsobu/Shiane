73deb917b001ce67be8a104fc8f7763ee7f732b0
oalonsobu <oalonsobu@uoc.edu>
2019-05-05 17:26:00 +0200

    Clouds added

--- Shiane/Assets/Scripts/GameControllers/ParallaxController.cs
+++ Shiane/Assets/Scripts/GameControllers/ParallaxController.cs
@@ -1,23 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class ParallaxController : MonoBehaviour
 {
-    
-    private List<SpriteRenderer> parallaxLayers;
     [SerializeField] GameObject backLayer;
     [SerializeField] GameObject frontLayer;
+    [SerializeField] GameObject cloudLayer;
     Vector3 mainCameraLastPosition;
+    float cloudLayerSpeed = 6.5f;
     float frontLayerSpeed = 3.5f;
     float backLayerSpeed  = 2f;
+
+    private List<GameObject> alreadyClonedClouds;
     
     // Start is called before the first frame update
     void Start()
     {
+        alreadyClonedClouds = new List<GameObject>();
         mainCameraLastPosition = Camera.main.transform.position;
         InitLayer(backLayer);
         InitLayer(frontLayer);
+        InitCloudLayer();
     }
 
     // Update is called once per frame
@@ -25,6 +31,7 @@ public class ParallaxController : MonoBehaviour
     {
         UpdateLayer(backLayer, backLayerSpeed);
         UpdateLayer(frontLayer, frontLayerSpeed);
+        UpdateCloudLayer(cloudLayerSpeed * 3);
         
         mainCameraLastPosition = Camera.main.transform.position;
     }
@@ -57,6 +64,25 @@ public class ParallaxController : MonoBehaviour
             counter++;
         }
     }
+    
+    //This is a bit different
+    void InitCloudLayer()
+    {
+        float maxYPosition    = cloudLayer.transform.position.y + Camera.main.orthographicSize;
+        float cloudNumber     = cloudLayer.transform.childCount;
+        float halfCameraXSize = (Camera.main.orthographicSize * Screen.width / Screen.height) / cloudNumber;
+        int counter           = 0;
+        foreach (Transform child in cloudLayer.transform)
+        {
+            float extents = child.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float xMin = halfCameraXSize * 2 * counter;
+            float xMax = halfCameraXSize * 2 * (counter + 1) - extents/2;
+            float xPos = Random.Range(xMin, xMax);
+            float yPos = Random.Range(cloudLayer.transform.position.y, maxYPosition);
+            child.position = new Vector3((cloudLayer.transform.position.x - halfCameraXSize) + xPos, yPos, cloudLayer.transform.position.z);
+            counter++;
+        }
+    }
 
     void UpdateLayer(GameObject layer, float speed)
     {
@@ -74,6 +100,54 @@ public class ParallaxController : MonoBehaviour
                 currentPos -= 4 * extents;
             }
             child.position = new Vector3(currentPos, child.position.y, child.position.z);
+        }
+    }
+    
+    void UpdateCloudLayer(float speed)
+    {
+        float cameraDistanceTravelled = mainCameraLastPosition.x - Camera.main.transform.position.x;
+        foreach (Transform child in cloudLayer.transform)
+        {
+            float extents = child.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float currentPos = child.position.x;
+            currentPos += Time.deltaTime * speed * cameraDistanceTravelled;
+            if (IsInLeftSideScreen(currentPos, extents) && cameraDistanceTravelled < 0)
+            {
+                if (child.GetComponent<SpriteRenderer>().enabled)
+                {
+                    InstantiateCloud(child.gameObject);
+                    child.GetComponent<SpriteRenderer>().enabled = false;
+                }
+            } else if (IsInRightSideScreen(currentPos, extents) && cameraDistanceTravelled > 0)
+            {
+                if (child.GetComponent<SpriteRenderer>().enabled)
+                {
+                    child.GetComponent<SpriteRenderer>().enabled = false;
+                }
+            }
+            else if (!IsInLeftSideScreen(currentPos, extents) && !IsInRightSideScreen(currentPos, extents))
+            {
+                child.GetComponent<SpriteRenderer>().enabled = true;
+            }
+            child.position = new Vector3(currentPos, child.position.y, child.position.z);
+        }
+    }
+
+    void InstantiateCloud(GameObject original)
+    {
+        if (!alreadyClonedClouds.Contains(original))
+        {
+            //Calculates all variables to get the new position to clone the cloud (randomly)
+            GameObject cloud = Instantiate(original, cloudLayer.transform);
+            float maxYPosition = cloudLayer.transform.position.y + Camera.main.orthographicSize;
+            float screenRatio = Camera.main.orthographicSize * Screen.width / Screen.height;
+            float extents = original.GetComponent<SpriteRenderer>().bounds.extents.x;
+            float minX = Camera.main.transform.position.x + screenRatio + extents;
+        
+            float yPos = Random.Range(cloudLayer.transform.position.y, maxYPosition);
+            float xPos = Random.Range(minX, minX + 0.25f);
+            cloud.transform.position = new Vector3(xPos, yPos, cloudLayer.transform.position.z);
+            alreadyClonedClouds.Add(original);
         }
     }
 }
