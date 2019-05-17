1cb77bd2c2276cca9e7bc0cc3fc68fd91180f696
oalonsobu <oalonsobu@uoc.edu>
2019-05-17 18:17:15 +0200

    Coldown on fireballs

--- Shiane/Assets/Scripts/Player/FirePowerController.cs
+++ Shiane/Assets/Scripts/Player/FirePowerController.cs
@@ -7,13 +7,34 @@ public class FirePowerController : MonoBehaviour
     [SerializeField] GameObject fireballPrefab;
 
     bool shootInCD = false;
-    
+    float cdTime = 1.5f;
+    float currentcdTime = 0;
+
     void Update()
+    {
+        UpdateCD();
+        Shoot();
+    }
+
+    void UpdateCD()
+    {
+        if (currentcdTime < cdTime)
+        {
+            currentcdTime += Time.deltaTime;
+        }
+        else if (shootInCD)
+        {
+            shootInCD = false;
+        }
+    }
+
+    void Shoot()
     {
         bool shoot = Input.GetAxis("Fire1") > 0;
         if (shoot && !shootInCD)
         {
             shootInCD = true;
+            currentcdTime = 0;
             Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             var dir = position - transform.position;
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
