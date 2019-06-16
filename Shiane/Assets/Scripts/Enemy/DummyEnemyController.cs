d455a0581ed93f77dabfd35fcda4a37c66075617
oalonsobu <oalonsobu@uoc.edu>
2019-06-16 10:15:58 +0200

    Minor fixes and decoration background level1

---
+++ Shiane/Assets/Scripts/Enemy/DummyEnemyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyEnemyController : MonoBehaviour
+{
+    
+    public void Attack()
+    {
+        //Do nothing. Avoid AnimationEvent has no receiver error
+    }
+     
+    public void StopAttacking()
+    {
+        //Do nothing. Avoid AnimationEvent has no receiver error
+    }
+}
